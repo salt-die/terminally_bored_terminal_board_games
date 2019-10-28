74d80fa77e92dc2c7fc28edb4055c034b1b64c47
salt-die <naclmolecule@gmail.com>
2019-10-27 21:06:33 -0500

    flags added

--- minesweeper.py
+++ minesweeper.py
@@ -18,6 +18,7 @@ class MineSweeper:
         self.count = np.where(self.minefield==1, -1, convolve(self.minefield, KERNEL,
                                                               mode='constant'))
         self.revealed = np.zeros(dim, dtype=bool)
+        self.flags = np.zeros(dim, dtype=bool)
 
     def place_mines(self):
         for _ in range(self.mines):
@@ -28,13 +29,17 @@ class MineSweeper:
                     break
 
     def ask(self):
-        x, y = input("Enter row, column: ")
-        return self.labels.find(x), self.labels.find(y)
+        flag = False
+        if input("Flag?: [y] ").lower()[:1] == "y":
+            flag = True
+        y, x = input("Enter row, column: ")
+        return flag, (self.labels.find(y), self.labels.find(x))
 
     def show(self):
-        view = np.where(self.revealed, self.count, -2)
+        view = np.where(self.flags, -3, np.where(self.revealed, self.count, -2))
+
         print(f'  {" ".join(self.labels[:self.WIDTH])}',
-              *(f'{label} {" ".join(" 12345678.X"[value] for value in row)}'
+              *(f'{label} {" ".join("▢12345678◄▣X"[value] for value in row)}'
                 for label, row in zip(self.labels, view)), sep="\n")
 
     def reveal(self, location):
@@ -52,12 +57,17 @@ class MineSweeper:
 
     def is_inbounds(self, location):
         y, x = location
-        return 0 <= x < self.WIDTH and 0 <= y < self.HEIGHT
+        return 0 <= y < self.HEIGHT and 0 <= x < self.WIDTH
 
     def start(self):
         self.show()
         while not self.GAMEOVER:
-            self.reveal(self.ask())
+            flag, location = self.ask()
+            print(flag, location)
+            if flag:
+                self.flags[location] = not self.flags[location]
+            else:
+                self.reveal(location)
             self.show()
         print("Gameover")
 
